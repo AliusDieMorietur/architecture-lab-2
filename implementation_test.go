6e007122ee673eec8455f983ee599c9a5ccb9f98
kirill <kirill.yemets@gmail.com>
2021-11-05 11:08:14 +0200

    Use gocheck instead of testify (according to variant)

--- implementation_test.go
+++ implementation_test.go
@@ -4,14 +4,31 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "gopkg.in/check.v1"
 )
 
-func TestPrefixToInfix(t *testing.T) {
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+// These are tests to test how gocheck works
+
+func (s *MySuite) TestPass(c *C) {
+	c.Assert(42, Equals, 42)
+}
+
+func (s *MySuite) TestFail(c *C) {
+	c.Assert(42, Equals, 43)
+}
+
+func (s *MySuite) TestPrefixToInfix(c *C) {
 	res, err := PrefixToInfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+
+	c.Assert(err, Not(Equals), nil)
+	c.Assert(res, Equals, "5 + (4 - 2) * 3")
 }
 
 func ExamplePrefixToInfix() {
@@ -19,5 +36,5 @@ func ExamplePrefixToInfix() {
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 2 + 2
 }
