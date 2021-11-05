77f9909c7cafdf3c0fa48a9aab287b158023aab8
kirill <kirill.yemets@gmail.com>
2021-11-05 13:52:20 +0200

    Added PrefixToInfix implementation and tests

--- implementation.go
+++ implementation.go
@@ -1,9 +1,73 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // TODO: document this function.
 // PrefixToInfix converts
 func PrefixToInfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	var stack []string
+	const operators = "+-*/"
+
+	if len(input) == 0 {
+		return "", fmt.Errorf("Empty string")
+	}
+
+	// fmt.Println("input string: ", input)
+
+	var inputs = strings.Split(input, " ")
+
+	var literal = "" // Current character (number or operator)
+	var e1 = ""      // first top value from stack
+	var e2 = ""      // second
+
+	// Go from right to left
+	for i := len(inputs) - 1; i >= 0; i-- {
+		literal = inputs[i]
+
+		if !strings.Contains(operators, literal) {
+			stack = append(stack, literal)
+		} else {
+
+			// Если в стеке не хватает элементов, значит опратор не в нужном месте (или лишний)
+			if len(stack) < 2 {
+				return "", fmt.Errorf("Wrong Expression: " + input)
+			}
+
+			e1, stack = Pop(stack)
+			e2, stack = Pop(stack)
+
+			// Поставить скобочки, если надо (если не надо, не ставить)
+			if strings.Contains(e1, " ") && strings.Contains("*/", literal) {
+				e1 = "(" + e1 + ")"
+			}
+			if strings.Contains(e2, " ") && strings.Contains("*/", literal) {
+				e2 = "(" + e2 + ")"
+			}
+
+			var new = e1 + " " + literal + " " + e2
+			stack = append(stack, new)
+		}
+
+		// fmt.Println(stack)
+	}
+
+	var res = ""
+	res, stack = Pop(stack)
+
+	if len(stack) > 0 {
+		return "", fmt.Errorf("Wrong Expression: " + input)
+	}
+
+	return res, nil
+}
+
+func Pop(s []string) (string, []string) {
+	if len(s) == 0 {
+		return "", []string{}
+	}
+
+	return s[len(s)-1], s[:len(s)-1]
 }
--- implementation_test.go
+++ implementation_test.go
@@ -14,27 +14,77 @@ type MySuite struct{}
 
 var _ = Suite(&MySuite{})
 
-// These are tests to test how gocheck works
+// Test Examples:
 
-func (s *MySuite) TestPass(c *C) {
-	c.Assert(42, Equals, 42)
-}
+// func (s *MySuite) TestPass(c *C) {
+// 	c.Assert(42, Equals, 42)
+// }
 
-func (s *MySuite) TestFail(c *C) {
-	c.Assert(42, Equals, 43)
-}
+// func (s *MySuite) TestFail(c *C) {
+// 	c.Assert(42, Equals, 43)
+// }
 
-func (s *MySuite) TestPrefixToInfix(c *C) {
+func (s *MySuite) TestPrefixToInfixDefault(c *C) {
 	res, err := PrefixToInfix("+ 5 * - 4 2 3")
 
-	c.Assert(err, Not(Equals), nil)
+	c.Assert(err, Equals, nil)
 	c.Assert(res, Equals, "5 + (4 - 2) * 3")
 }
 
+func (s *MySuite) TestPrefixToInfix222(c *C) {
+	res, err := PrefixToInfix("+ 2 * 2 2")
+
+	c.Assert(err, Equals, nil)
+	c.Assert(res, Equals, "2 + 2 * 2")
+}
+
+func (s *MySuite) TestPrefixToInfixMultiply(c *C) {
+	res, err := PrefixToInfix("* 7 - 1 * 13 4")
+
+	c.Assert(err, Equals, nil)
+	c.Assert(res, Equals, "7 * (1 - 13 * 4)")
+}
+
+func (s *MySuite) TestPrefixToInfixLong(c *C) {
+	res, err := PrefixToInfix("+ 11 + 22 + 33 + 4 + 5 + 6 + 7 8")
+
+	c.Assert(err, Equals, nil)
+	c.Assert(res, Equals, "11 + 22 + 33 + 4 + 5 + 6 + 7 + 8")
+}
+
+func (s *MySuite) TestPrefixToInfixLong2(c *C) {
+	res, err := PrefixToInfix("/ + * 14 - 15 7 * 3 17 + 10 1")
+
+	c.Assert(err, Equals, nil)
+	c.Assert(res, Equals, "(14 * (15 - 7) + 3 * 17) / (10 + 1)")
+}
+
+func (s *MySuite) TestPrefixToInfixEmptyString(c *C) {
+	res, err := PrefixToInfix("")
+
+	c.Assert(err, Not(Equals), nil)
+	c.Assert(err.Error(), Equals, "Empty string")
+	c.Assert(res, Equals, "")
+}
+
+func (s *MySuite) TestPrefixToInfixExtraNumber(c *C) {
+	res, err := PrefixToInfix("* 7 - 1 * 13 4 7")
+
+	c.Assert(err, Not(Equals), nil)
+	c.Assert(res, Equals, "")
+}
+
+func (s *MySuite) TestPrefixToInfixFewNumbers(c *C) {
+	res, err := PrefixToInfix("- * 2 2")
+
+	c.Assert(err, Not(Equals), nil)
+	c.Assert(res, Equals, "")
+}
+
 func ExamplePrefixToInfix() {
-	res, _ := PrefixToInfix("+ 2 2")
+	res, _ := PrefixToInfix("* + 2 - 5 8 - 101 / 36 6")
 	fmt.Println(res)
 
 	// Output:
-	// 2 + 2
+	// (2 + 5 - 8) * (101 - 36 / 6)
 }
